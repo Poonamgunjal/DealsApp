192771f8041a4c06ebf01188f3abb82463943d2a
Poonam Gunjal <Poonam.Gunjal@rockwellautomation.com>
2021-08-28 16:06:50 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SimpleTabs from './components/header';
+function App() {
+  return (
+    <div className="App">
+      <SimpleTabs />
+    </div>
+  );
+}
+
+export default App;
